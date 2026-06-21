33f4ae085a7c9fdd600546b0b4c900d32b6b4bb9
Mohamed <mohamedmohamed1812@gmail.com>
2026-06-21 19:11:14 +0100

    Fix: Changed the recordshop table structure so it uses mysql auto-increment on ID. NOTE: No need to use update-database on EF. Tables are now created when I do 'dotnet-run'

---
+++ RecordShop/Migrations/20260621174727_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "MusicRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RecordTitle = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Artists = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ReleaseYear = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Genre = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Cover = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MusicRecords", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MusicRecords");
+        }
+    }
+}
---
+++ RecordShop/MyDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace RecordShop
+{
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+
+    public class MyDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+
+        public MyDbContext CreateDbContext(string[] args)
+        {
+
+            var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+
+            var connectionString = "server=localhost;database=recordshop;user=root;password=placeholder;";
+            
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 36));
+            
+            optionsBuilder.UseMySql(connectionString,serverVersion);
+
+            return new MyDbContext(optionsBuilder.Options);
+        }
+
+    }
+}
