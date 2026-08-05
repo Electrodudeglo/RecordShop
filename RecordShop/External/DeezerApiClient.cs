c3e0594a7d187965ad945c88ee7d8544a13b1632
Mohamed Lenovo Laptop <mohamedmohamed1812@gmail.com>
2026-08-05 15:54:20 +0100

    Add: Add External folder with Api Call and Model

---
+++ RecordShop/External/DeezerApiClient.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Web;
+
+namespace RecordShop.External
+{
+    public class DeezerApiClient
+    {
+        private readonly HttpClient _http;
+
+        public DeezerApiClient(HttpClient http)
+        {
+            _http = http;
+        }
+
+
+        public async Task<DeezerApiResponse> FindAlbumAsync(string title, string artist)
+        {
+            try 
+            {
+                var query = HttpUtility.UrlDecode($"{title} {artist}");
+                var searchUrl = $"search/album?q={query}&limit=1";
+
+                var response = await _http.GetAsync(searchUrl);
+
+                if(!response.IsSuccessStatusCode)
+                {
+
+                    return new DeezerApiResponse
+                    {
+                        Status = MapStatus(response.StatusCode),
+                        Album = null
+                    };
+                }
+
+                var searchResults = await response.Content.ReadFromJsonAsync<DeezerSearchResult>();
+
+                if (searchResults?.Data == null || searchResults.Data.Count == 0)
+                {
+                    return new DeezerApiResponse
+                    {
+                        Status = DeezerResultStatusEnum.NotFound,
+                        Album = null
+                    };
+                }
+
+
+            }
+            catch
+            {
+            }           
+        }
+
+        private DeezerResultStatusEnum MapStatus(HttpStatusCode code)
+        {
+            return code switch
+            {
+                HttpStatusCode.NotFound => DeezerResultStatusEnum.NotFound,
+                HttpStatusCode.InternalServerError => DeezerResultStatusEnum.ServerError,
+                _ => DeezerResultStatusEnum.ServerError
+            };
+        }
+
+
+    }
+   
+    }
+
---
+++ RecordShop/External/DeezerApiModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+namespace RecordShop.External
+{
+    public class DeezerApiModel
+    {
+
+    }
+
+    public enum DeezerResultStatusEnum
+{
+    Success,
+    NotFound,
+    ServerError,
+    NetworkError,
+    InvalidJson
+}
+
+public class DeezerApiResponse
+{
+    public DeezerResultStatusEnum Status { get; set; }
+    public DeezerAlbumDetails? Album { get; set; }
+}
+
+public class DeezerSearchResult
+{
+    public List<DeezerAlbumSummary> Data { get; set; } = new();
+}
+
+public class DeezerAlbumSummary
+{
+    public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+}
+
+public class DeezerAlbumDetails
+{
+    public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Cover { get; set; } = String.Empty;
+        public string Artist { get; set; } = String.Empty;
+}
+
+
+}
